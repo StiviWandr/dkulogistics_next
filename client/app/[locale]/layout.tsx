e6916f8d8d648b15be0f04c74a99409251b995c1
Артур Роднов <artur03rodnov@gmail.com>
2023-10-17 23:59:20 +0600

    Test: local

--- client/app/[locale]/layout.tsx
+++ client/app/[locale]/layout.tsx
@@ -1,10 +1,13 @@
+'use client'
 import { Header } from '@/UI/Header/Header'
-import './globals.css'
+import '@/globals.css'
 import type { Metadata } from 'next'
+
 import { Montserrat } from 'next/font/google'
 import { Footer } from '@/UI/Footer/Footer'
 import { ReduxProvider } from '@/Store/ReduxProvider'
-
+import {notFound} from 'next/navigation';
+import { NextIntlClientProvider } from 'next-intl/client'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -12,8 +15,18 @@ export const metadata: Metadata = {
     title: 'DKU - логистика',
     description: 'Журнал по всем тема в мире логистика от Универстита КНУ',
 }
-  
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export function generateStaticParams() {
+    return [{locale: 'en'}, {locale: 'de'}];
+  }
+   
+  export default async function LocaleLayout({children, params: {locale}}: any) {
+    let messages;
+    try {
+      messages = (await import(`../../messages/${locale}.json`)).default;
+    } catch (error) {
+      notFound();
+    }
+   
     return (
         <html lang={'ru'}>
             <body className={montserrat.className}>
@@ -21,7 +34,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
                     <div className='wrapper'>
                         <Header />
                         <main className='main'>
+                        <NextIntlClientProvider locale={locale} messages={messages}>
                             {children}
+                        </NextIntlClientProvider>
                         </main>
                         <Footer></Footer>
                     </div>
--- client/src/Modules/HomePage/AboutJournal/AboutJournal.tsx
+++ client/src/Modules/HomePage/AboutJournal/AboutJournal.tsx
@@ -1,15 +1,16 @@
 import { Text32 } from '@/UI/Text32/Text32';
 import styles from './AboutJournal.module.css'
+import { useTranslations } from 'next-intl';
 
 interface IAboutJournalProps {
     lang: any
 }
 
 export async function AboutJournal (props: IAboutJournalProps) {
-
+    const t = useTranslations('pages')
     return (
         <div>
-            <Text32>О журнале</Text32>
+            <Text32>{t('home.about')}</Text32>
         </div>
     );
 }
