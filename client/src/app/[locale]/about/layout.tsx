8a9191d59d2bea79c539177a059186162849ea82
Artur <artur03rodnov@gmail.com>
2024-01-08 03:02:54 +0600

    Fix: ssg into isr on About

--- client/src/app/[locale]/about/layout.tsx
+++ client/src/app/[locale]/about/layout.tsx
@@ -1,19 +1,14 @@
-import { Header } from '@/UI/Header/Header'
+"use client"
 import '@/app/globals.css'
-import type { Metadata } from 'next'
 import { Layout } from 'antd';
-import { AboutSidebar } from '@/Modules/AboutPage/AboutSidebar/AboutSidebar';
-import initTranslations from '@/app/i18n';
+
 import { Content } from 'antd/es/layout/layout';
 import { Container } from '@/UI/Container/Container';
 
-export default async function LocaleLayout({children, params: {locale}}: any) {
-    const { t, options } = await initTranslations(locale, ['home']);
-
+export default function LocaleLayout({children, params: {locale}}: any) {
     return (
 
         <Layout style={{height: '100%', background: 'white'}} >
-            {/* <AboutSidebar/> */}
             <Container>
                 <Layout className="site-layout" style={{height: '100%', background: 'white'}}>
                     
